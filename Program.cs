76c81ab59c809981cda70bdf849eaf811f72e2c5
Farooqi Suhail <suhail.for.learning.gmail.com>
2026-05-17 22:26:53 +0530

    Add Person class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using hello;
+
+
+var objPerson = new Person
+{
+    Id = 1,
+    FirstName = "Umar",
+    LastName = "Farooqi"
+};
+
+Console.WriteLine($"Hello {objPerson.FirstName} {objPerson.LastName}.");
+
